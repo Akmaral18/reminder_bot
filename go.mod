846ba6d8fbf14a8d12d2c7021bfa9494bd6fd598
akmaral.orizakunova@gmail.com <akmaral.orizakunova@gmail.com>
2025-11-01 21:16:41 +0600

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module reminder_bot
+
+go 1.22.12
+
+require github.com/go-chi/chi v1.5.5
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+func main() {
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World!")
+	})
+	fmt.Println("Запускаем сервер")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Завершаем работу")
+}
